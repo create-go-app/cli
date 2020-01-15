b94abe8c64b73688a7f624e61307f0dfda234e4e
Vic Shóstak <vikkyshostak@gmail.com>
2020-01-15 21:18:09 +0300

    Update file.go

--- internal/cgapp/file.go
+++ internal/cgapp/file.go
@@ -7,24 +7,24 @@ import (
 	"path/filepath"
 )
 
-// File will print any string of text to a file safely by
-// checking for errors and syncing at the end.
+// File function will print any string of text to a file safely
+// by checking for errors and syncing at the end
 func File(name, data string) error {
-	//
+	// Define folder
 	folder := filepath.Join(appPath, name)
 
-	//
+	// Create file
 	file, err := os.Create(folder)
 	ErrChecker(err)
 	defer file.Close()
 
-	//
+	// Write to created file
 	_, err = io.WriteString(file, data)
 	ErrChecker(err)
 
-	// Show report for each file
+	// Show report for file
 	fmt.Printf("%v[OK]%v File '%v' was created!\n", green, noColor, name)
 
-	//
+	// Sync & return
 	return file.Sync()
 }
