09d29bdeaf0be3f9d0ff821e9f22de42d8040a0b
Vic Shóstak <vikkyshostak@gmail.com>
2020-01-22 00:03:07 +0300

    Refactoring embed box pkg

--- configs/defaults.go
+++ configs/defaults.go
@@ -10,6 +10,21 @@ func newConfigsBox() *configsBox {
 	return &configsBox{storage: make(map[string][]byte)}
 }
 
+// Add a file to box
+func (r *configsBox) Add(file string, content []byte) {
+	r.storage[file] = content
+}
+
+// Get file's content
+// Always use / for looking up
+// For example: /init/README.md is actually configs/init/README.md
+func (r *configsBox) Get(file string) []byte {
+	if f, ok := r.storage[file]; ok {
+		return f
+	}
+	return nil
+}
+
 // Find for a file
 func (r *configsBox) Has(file string) bool {
 	if _, ok := r.storage[file]; ok {
@@ -18,32 +33,17 @@ func (r *configsBox) Has(file string) bool {
 	return false
 }
 
-// Get file's content
-// Always use / for looking up
-// For example: /init/README.md is actually resources/init/README.md
-func (r *configsBox) Get(file string) []byte {
-	if f, ok := r.storage[file]; ok {
-		return f
-	}
-	return nil
-}
-
-// Add a file to box
-func (r *configsBox) Add(file string, content []byte) {
-	r.storage[file] = content
-}
-
 // Configs expose
 var configs = newConfigsBox()
-
-// Get a file from box
-func Get(file string) []byte {
-	return configs.Get(file)
-}
 
 // Add a file content to box
 func Add(file string, content []byte) {
 	configs.Add(file, content)
+}
+
+// Get a file from box
+func Get(file string) []byte {
+	return configs.Get(file)
 }
 
 // Has a file in box
