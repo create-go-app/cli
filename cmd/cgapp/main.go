c15207ffec7b9463d9c465ec9e13ec9300475603
Vic Shóstak <vikkyshostak@gmail.com>
2020-01-22 14:01:14 +0300

    Draft v0.6.0b1

--- cmd/cgapp/main.go
+++ cmd/cgapp/main.go
@@ -1,26 +1,20 @@
 package main
 
-import (
-	"github.com/create-go-app/cli/internal/cgapp"
-)
+import "github.com/create-go-app/cli/internal/cgapp"
 
 var (
 	// cgapp CLI version
-	version string = "0.5.4"
+	version string = "0.6.0b1"
 
 	// Templates registry
 	registry = map[string]string{
 		// Backend templates
 		"net/http": "create-go-app/net_http-go-template",
 		"echo":     "create-go-app/echo-go-template",
-		// "gin":      "create-go-app/gin-go-template",
-		// "iris":     "create-go-app/iris-go-template",
 
 		// Frontend templates
 		"react":  "create-go-app/react-js-template",
 		"preact": "create-go-app/preact-js-template",
-		// "vue":      "create-go-app/vue-js-template",
-		// "svelte":   "create-go-app/svelte-js-template",
 
 		// Docker containers
 		"nginx":    "create-go-app/nginx-certbot-docker",
