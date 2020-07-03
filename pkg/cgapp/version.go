5ed1c71b4049dbc133120348d009daa9fed07872
Vic Shóstak <vikkyshostak@gmail.com>
2020-07-04 01:00:39 +0300

    Bump v1.2.1

--- pkg/cgapp/version.go
+++ pkg/cgapp/version.go
@@ -2,18 +2,19 @@ package cgapp
 
 var (
 	// cgapp CLI version
-	version = "1.2.0"
+	version = "1.2.1"
 
 	// Templates registry
 	registry = map[string]string{
 		// Backend templates
 		"net/http": "create-go-app/net_http-go-template",
 		"fiber":    "create-go-app/fiber-go-template",
+		"echo":     "create-go-app/echo-go-template",
 
 		// Frontend templates
 		"react-js": "create-go-app/react-js-template",
-		"react-ts": "create-go-app/react-ts-template",
 		"preact":   "create-go-app/preact-js-template",
+		"react-ts": "create-go-app/react-ts-template",
 
 		// Docker containers
 		"nginx":    "create-go-app/nginx-docker",
