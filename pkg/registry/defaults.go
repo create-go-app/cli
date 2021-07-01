02ad4ed8ab7a88527c4b7eb647e1bf50d94e2ce3
Vic Shóstak <vic@shostak.dev>
2021-07-01 08:48:08 +0300

    Update AlecAivazis/survey to v2.2.13

--- pkg/registry/defaults.go
+++ pkg/registry/defaults.go
@@ -11,7 +11,7 @@ import (
 )
 
 // CLIVersion version of Create Go App CLI.
-const CLIVersion string = "2.2.2"
+const CLIVersion string = "2.2.3"
 
 // Variables struct for Ansible variables (inventory, hosts).
 type Variables struct {
