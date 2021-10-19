4fef10bb5948f95916bb1156df6402228577032a
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2021-10-19 14:19:05 +0900

    Support Windows colors

--- pkg/cgapp/utils.go
+++ pkg/cgapp/utils.go
@@ -7,9 +7,12 @@ package cgapp
 import (
 	"fmt"
 	"time"
+
+	"github.com/mattn/go-colorable"
 )
 
-// ShowMessage function for send message to output.
+var out = colorable.NewColorableStdout()
+
 func ShowMessage(level, text string, startWithNewLine, endWithNewLine bool) {
 	// Define variables.
 	var startLine, endLine string
@@ -22,7 +25,7 @@ func ShowMessage(level, text string, startWithNewLine, endWithNewLine bool) {
 		endLine = "\n" // set a new line
 	}
 
-	fmt.Println(startLine + colorizeLevel(level) + text + endLine)
+	fmt.Fprintln(out, startLine+colorizeLevel(level)+text+endLine)
 }
 
 // ShowError function for send error message to output.
