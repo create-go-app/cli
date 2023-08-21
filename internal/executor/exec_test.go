aaa4b5389244d7fe4312518b3a021d0425355cd4
Vic Shóstak <vikkyshostak@gmail.com>
2023-08-21 11:20:33 +0300

    Fix Execute() tests

--- internal/executor/exec_test.go
+++ internal/executor/exec_test.go
@@ -4,9 +4,8 @@ import "testing"
 
 func TestExecCommand(t *testing.T) {
 	type args struct {
-		command    string
-		options    []string
-		silentMode bool
+		command string
+		options []string
 	}
 	tests := []struct {
 		name    string
@@ -16,31 +15,24 @@ func TestExecCommand(t *testing.T) {
 		{
 			"successfully executing command",
 			args{
-				command:    "echo",
-				options:    []string{"ping"},
-				silentMode: false,
+				command: "echo",
+				options: []string{"ping"},
 			},
 			false,
 		},
 		{
 			"successfully executing command with silent mode",
 			args{
-				command:    "echo",
-				options:    []string{"ping"},
-				silentMode: true,
+				command: "echo",
+				options: []string{"ping"},
 			},
 			false,
-		},
-		{
-			"failed executing command",
-			args{},
-			true,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if err := ExecCommand(tt.args.command, tt.args.options, tt.args.silentMode); (err != nil) != tt.wantErr {
-				t.Errorf("ExecCommand() error = %v, wantErr %v", err, tt.wantErr)
+			if err := Execute(tt.args.command, tt.args.options...); (err != nil) != tt.wantErr {
+				t.Errorf("Execute() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
 	}
